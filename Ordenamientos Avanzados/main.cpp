9feae2b78de5e9997a764bc353776504a68e079a
Andres Torres <atorres@applab.mx>
2021-09-09 10:55:05 -0500

    QuickSort

--- Ordenamientos Avanzados/main.cpp
+++ Ordenamientos Avanzados/main.cpp
@@ -62,11 +62,46 @@ void mergeSort(int inicio,int fin, int arreglo[],int tamanioArreglo){
     }
 }
 
+void intercambia(int* a, int* b){
+    int t=*a;
+    *a=*b;
+    *b=t;
+}
+
+
+int partir(int inicio,int fin,int pivote,int arreglo[]){
+    cout<<"Entra al método partir "<<endl;
+    int elementoPivote=arreglo[pivote]; 
+    int j=inicio;
+    for(int i=inicio;i<=fin;i++){
+        if(arreglo[i]<elementoPivote){
+            j++;
+            cout<<"Intercambia I: "<<arreglo[i]<<" con J: "<<arreglo[j]<<endl;
+            intercambia(&arreglo[i],&arreglo[j]);
+        }
+    }
+    pivote=j;
+    cout<<" Intercambia inicio: "<<arreglo[inicio]<<" con pivote: "<<arreglo[pivote]<<endl;
+    intercambia(&arreglo[inicio],&arreglo[pivote]);
+    return pivote;
+}
+
+void quickSort(int inicio, int fin, int arreglo[]){
+    if(inicio<fin){
+        int pivote=partir(inicio,fin,inicio,arreglo);
+        quickSort(inicio,pivote-1,arreglo);
+        quickSort(pivote+1,fin,arreglo);
+    }
+}
+
 
 int main(){
-    int arreglo[]={3,1,7,5,10,8,6,11,13,4,0};
+    int arreglo[]={5,7,3,4,10,2,1,8,9};
+    //Debería quedar en la primera llamada [1,3,4,2,5,7,10,8,9]
+    // Llamada recursiva izquierda con [1,3,4,2]-> [1,3,4,2]-> Llamada recursiva con [1] y con [3,4,2]-> [2,3,4] -> Llamada recursiva [2] y [4] -> [1,2,3,4]
+    //Llamada recursiva derecha con [7,10,8,9]-> Llamada recursiva con [7] y [10,8,9]-> [9,8,10] -> Llamada recursiva con [9,8] y [10]-> [8,9]-> [8,9,10]-> [7,8,9,10]
     int tamanioArreglo=sizeof(arreglo)/sizeof(arreglo[0]);
-    mergeSort(0,tamanioArreglo-1,arreglo,tamanioArreglo);
+    quickSort(0,tamanioArreglo-1,arreglo);
     imprimirArreglo(arreglo,tamanioArreglo);
     return 0;
 }
