c6a63753c2e753c539684981d96d856efb637732
Andres Torres <atorres@applab.mx>
2021-11-18 10:15:36 -0600

    Comentando codigo

--- Hashing/Hashing.h
+++ Hashing/Hashing.h
@@ -19,24 +19,23 @@ Hashing::Hashing(){
 }
 
 void Hashing::agregarElemento(int elemento){
-    int pos=calcularHash(elemento);
-    if(tabla[pos]!=0){
-        int inicial=pos;
-        while(tabla[inicial]!=0){
-            inicial++;
-            if(inicial==11){
-                inicial=0;
+    int pos=calcularHash(elemento);//Calcular la pos en la que debería entrar el elemento a la tabla 
+    if(tabla[pos]!=0){ //Si la pos esta no vacía quiere decir que hay colisión
+        int inicial=pos; //igualo inicial a la pos que en teoría le toca pero colisionó 
+        while(tabla[inicial]!=0){//Recorro mientras no encuentre una pos vacía para insertar el elemento
+            inicial++;//incremento el índice del recorrido
+            if(inicial==11){//Si llegue a la ultima pos de la tabla -> debo regresar al inicio
+                inicial=0;//regreso al inicio (Solo si llegue a la última pos y no encontré ninguno vacío)
             }
-            if(inicial==pos){
-                cout<<"Tabla llena"<<endl;
+            if(inicial==pos){//Si mi recorrido llega la donde empecé quiere decir que NO hay pos vacías.
+                cout<<"Tabla llena"<<endl;//Paro el método imprimiendo que la tabla está llena
                 return;
             }
         }
-        tabla[inicial]=elemento;
+        tabla[inicial]=elemento; //Inserto el elemento en la primera pos vacía solo sí encontre una pos vacía 
     }else{
-        tabla[pos]=elemento;
+        tabla[pos]=elemento;//Si la pos está vacia inserto en esa pos
     }
-    
 }
 
 void Hashing::imprimirTabla(){
